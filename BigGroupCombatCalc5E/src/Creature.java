16647dbc2da0a8afed33b107a5d352a44d6a9783
EthanAYoung <youngethana@gmail.com>
2019-05-09 13:04:30 -0400

    first

---
+++ BigGroupCombatCalc5E/src/Creature.java
@@ -0,0 +1,6 @@
+
+public class Creature {
+	
+	int hp;
+
+}
---
+++ BigGroupCombatCalc5E/src/Runner.java
@@ -0,0 +1,15 @@
+
+public class Runner {
+
+	public static void main(String[] args) {
+		Creature[][] grid = new Creature[20][20];
+		grid = populateGridTest(grid);
+
+	}
+
+	private static Creature[][] populateGridTest(Creature[][] grid) {
+		
+		return null;
+	}
+
+}
